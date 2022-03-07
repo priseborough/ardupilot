c9e513d67393f405f75b01d259edc49d03a8fed0
Paul Riseborough <gncsolns@gmail.com>
2022-03-07 18:48:40 +1100

    ArduPlane: make stabilize go 15 deg nose down and don't windup integrators

--- ArduPlane/mode_stabilize.cpp
+++ ArduPlane/mode_stabilize.cpp
@@ -4,6 +4,8 @@
 void ModeStabilize::update()
 {
     plane.nav_roll_cd = 0;
-    plane.nav_pitch_cd = 0;
+    plane.nav_pitch_cd = -1500;
+    plane.pitchController.reset_I();
+    plane.rollController.reset_I();
 }
 
