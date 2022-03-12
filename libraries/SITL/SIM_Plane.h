19a09ba5b2e364b06c47400acd7a018d654a3b28
Paul Riseborough <gncsolns@gmail.com>
2022-03-12 20:57:39 +1100

    SITL: fix equivalent ail and elev to single surface scaling

--- libraries/SITL/SIM_Plane.h
+++ libraries/SITL/SIM_Plane.h
@@ -324,14 +324,14 @@ protected:
 
       // mixing from aileron and elevator to individual control surfaces (front right, front left, rear right, rear left)
       // assume 50% aileron differential is used
-      float ail_to_fr = - 0.125; // positive deflection in CFD model is TE down
-      float ail_to_fl = - 0.25; // positive deflection is CFD model TE up
-      float ail_to_rr = - 0.125; // positive deflection is CFD model TE down
-      float ail_to_rl = - 0.25; // positive deflection is CFD model TE up
-      float ele_to_fr = + 0.125; // positive deflection is CFD model TE down
-      float ele_to_fl = - 0.25; // positive deflection is CFD model TE up
-      float ele_to_rr = - 0.125; // positive deflection is CFD model TE down
-      float ele_to_rl = + 0.25; // positive deflection is CFD model TE up
+      float ail_to_fr = - 0.5; // positive deflection in CFD model is TE down
+      float ail_to_fl = - 1.0; // positive deflection is CFD model TE up
+      float ail_to_rr = - 0.5; // positive deflection is CFD model TE down
+      float ail_to_rl = - 1.0; // positive deflection is CFD model TE up
+      float ele_to_fr = + 0.5; // positive deflection is CFD model TE down
+      float ele_to_fl = - 1.0; // positive deflection is CFD model TE up
+      float ele_to_rr = - 0.5; // positive deflection is CFD model TE down
+      float ele_to_rl = + 1.0; // positive deflection is CFD model TE up
 
     } default_cfd_model;
 
