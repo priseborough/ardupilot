6056f5c196543609bd99cfbd9cb7a898bbac2f4d
Paul Riseborough <gncsolns@gmail.com>
2022-04-15 21:39:58 +1000

    AP_vehicle: include  pitch_trim_cd in list of shared parameters

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -121,6 +121,7 @@ public:
         AP_Int8  autotune_level;
         AP_Int8  stall_prevention;
         AP_Int16 loiter_radius;
+        AP_Int16 pitch_trim_cd;
 
         struct Rangefinder_State {
             bool in_range:1;
