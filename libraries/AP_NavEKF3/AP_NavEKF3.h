07880536a32553eb44e02e3e657a15308a1b4dbf
Paul Riseborough <gncsolns@gmail.com>
2022-06-26 08:47:08 +1000

    AP_NavEKF3: Add accessor function for airspeed health monitoring

--- libraries/AP_NavEKF3/AP_NavEKF3.h
+++ libraries/AP_NavEKF3/AP_NavEKF3.h
@@ -89,6 +89,10 @@ public:
     // returns false if estimate is unavailable
     bool getAirSpdVec(int8_t instance, Vector3f &vel) const;
 
+    // return the innovation in m/s, innovation variance in (m/s)^2 and age in msec of the last TAS measurement processed
+    // returns false if the data is unavilable
+    bool getAirSpdHealthData(float &innovation, float &innovationVariance, uint32_t &age_ms) const;
+
     // Return the rate of change of vertical position in the down direction (dPosD/dt) in m/s for the specified instance
     // An out of range instance (eg -1) returns data for the primary instance
     // This can be different to the z component of the EKF velocity state because it will fluctuate with height errors and corrections in the EKF
