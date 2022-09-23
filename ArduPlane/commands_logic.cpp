d7e87cfdd70ab343b24aa719d5bdd7cac94a9b7a
Paul Riseborough <gncsolns@gmail.com>
2022-09-23 10:35:46 +1000

    Plane: Hack to allow larger mission command waypoint acceptance distance

--- ArduPlane/commands_logic.cpp
+++ ArduPlane/commands_logic.cpp
@@ -614,7 +614,7 @@ bool Plane::verify_nav_wp(const AP_Mission::Mission_Command& cmd)
     Location flex_next_WP_loc = next_WP_loc;
 
     uint8_t cmd_passby = HIGHBYTE(cmd.p1); // distance in meters to pass beyond the wp
-    uint8_t cmd_acceptance_distance = LOWBYTE(cmd.p1); // radius in meters to accept reaching the wp
+    uint16_t cmd_acceptance_distance = 4 * LOWBYTE(cmd.p1); // radius in meters to accept reaching the wp
 
     if (cmd_passby > 0) {
         const float dist = prev_WP_loc.get_distance(flex_next_WP_loc);
