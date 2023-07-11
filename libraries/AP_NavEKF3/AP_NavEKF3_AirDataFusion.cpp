48496be17f0d683f1cb9874a93d4ed6b210b0453
Paul Riseborough <gncsolns@gmail.com>
2023-07-11 23:18:23 +1000

    AP_NavEKF3: re-work setWind processing to include vehicle velocity reset

--- libraries/AP_NavEKF3/AP_NavEKF3_AirDataFusion.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_AirDataFusion.cpp
@@ -768,13 +768,27 @@ bool NavEKF3_core::setWind(float speed, float direction)
     zeroRows(P,22,23);
     zeroCols(P,22,23);
 
-    // set the variances
+    // set the wind state variances
     P[22][22] = P[23][23] = sq(WIND_SPD_UNCERTAINTY);
 
     // reset the NE wind velocity states to the measurement
+    Vector2F wind_vel_prev = stateStruct.wind_vel;
     stateStruct.wind_vel.x = -speed * cosF(radians(direction));
     stateStruct.wind_vel.y = -speed * sinF(radians(direction));
 
+    // update the vehicle velocity states to be consistent with the new wind estimate
+    velResetNE = stateStruct.wind_vel - wind_vel_prev;
+    stateStruct.velocity.xy() += velResetNE;
+
+    // update the output buffer
+    for (uint8_t i=0; i<imu_buffer_length; i++) {
+        storedOutput[i].velocity.xy() = stateStruct.velocity.xy();
+    }
+    outputDataNew.velocity.xy() = stateStruct.velocity.xy();
+    outputDataDelayed.velocity.xy() = stateStruct.velocity.xy();
+
+    // store the time of the reset
+    lastVelReset_ms = imuSampleTime_ms;
     lastExtWindVelSet_ms = imuSampleTime_ms;
 
     return true;
