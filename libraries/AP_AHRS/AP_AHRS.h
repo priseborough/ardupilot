1053befef9d617009a3a3e6dfd43e67f92a39bd3
Paul Riseborough <gncsolns@gmail.com>
2023-09-29 19:51:25 +1000

    AP_AHRS: Re-work implementation of AHRS_OPTIONS
    Ensure that a fallback for planes will always occur if the EKF is unable to provide attitude, vertical velocity or vertical position and the DCM is.
    Add separate control over fallback behaviour in FW and VTOL modes
    
    # Conflicts:
    #	libraries/AP_AHRS/AP_AHRS.cpp

--- libraries/AP_AHRS/AP_AHRS.h
+++ libraries/AP_AHRS/AP_AHRS.h
@@ -849,7 +849,8 @@ private:
     void Write_POS(void) const;
 
     enum class Options : uint16_t {
-        DISABLE_DCM_FALLBACK=(1U<<0),
+        DISABLE_DCM_FALLBACK_FW=(1U<<0),
+        DISABLE_DCM_FALLBACK_VTOL=(1U<<1),
     };
     AP_Int16 _options;
 
