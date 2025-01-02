c716264801a5dcf55eb57b21ddfdf3808a8b915c
Paul Riseborough <gncsolns@gmail.com>
2025-01-02 14:00:56 +1100

    AP_NavEKF2: compatibility changes for writeExtNavData interface
    Only the horizontal position covariance is passed thgrough the interface

--- libraries/AP_NavEKF2/AP_NavEKF2.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2.cpp
@@ -1512,22 +1512,23 @@ void NavEKF2::updateLaneSwitchPosDownResetData(uint8_t new_primary, uint8_t old_
 /*
  * Write position and quaternion data from an external navigation system
  *
-     * pos        : position in the RH navigation frame. Frame is assumed to be NED if frameIsNED is true. (m)
-     * quat       : quaternion desribing the rotation from navigation frame to body frame
-     * posCov     ; Row-major representation of position 3x3 cross-covariance matrix upper right triangle (states: x_global, y_global, z_global; first three entries are the first ROW, next 2 entries are the second ROW, etc.). If position variances are unknown, assign NaN value to element [0].
-     * angErr     : 1-sigma spherical angle error (rad). Assign NaN value if not known.
-     * timeStamp_ms : system time the measurement was taken, not the time it was received (mSec)
-     * resetTime_ms : system time of the last position reset request (mSec)     *
+    * pos        : position in the RH navigation frame. Frame is assumed to be NED if frameIsNED is true. (m)
+    * quat       : quaternion desribing the rotation from navigation frame to body frame
+    * posCovNE   : Row-major representation of position 2x2 cross-covariance matrix upper right triangle (states: x_global, y_global; first two entries are the first ROW, next  entries is the second ROW). If position variances are unknown, assign NaN value to element [0].
+    * posErrD    : 1-sigma vertical position error (m). Assign NaN value if not known.
+    * angErr     : 1-sigma spherical angle error (rad). Assign NaN value if not known.
+    * timeStamp_ms : system time the measurement was taken, not the time it was received (mSec)
+    * resetTime_ms : system time of the last position reset request (mSec)     *
  *
  * Sensor offsets are pulled directly from the AP_VisualOdom library
  *
 */
-void NavEKF2::writeExtNavData(const Vector3f &pos, const Quaternion &quat, const float posCov[6], float angErr, uint32_t timeStamp_ms, uint32_t resetTime_ms)
+void NavEKF2::writeExtNavData(const Vector3f &pos, const Quaternion &quat, const float posCovNE[3], float posErrD, float angErr, uint32_t timeStamp_ms, uint32_t resetTime_ms)
 {
-    AP::dal().writeExtNavData(pos, quat, posCov, angErr, timeStamp_ms, resetTime_ms);
+    AP::dal().writeExtNavData(pos, quat, posCovNE, posErrD, angErr, timeStamp_ms, resetTime_ms);
     if (!option_is_set(Option::DisableExternalNav) && core) {
         for (uint8_t i=0; i<num_cores; i++) {
-            core[i].writeExtNavData(pos, quat, posCov, angErr, timeStamp_ms, resetTime_ms);
+            core[i].writeExtNavData(pos, quat, posCovNE, posErrD, angErr, timeStamp_ms, resetTime_ms);
         }
     }
 }
--- libraries/AP_NavEKF2/AP_NavEKF2_Measurements.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2_Measurements.cpp
@@ -935,10 +935,10 @@ void NavEKF2_core::updateTimingStatistics(void)
     timing.count++;
 }
 
-void NavEKF2_core::writeExtNavData(const Vector3f &pos, const Quaternion &quat, const float posCov[6], float angErr, uint32_t timeStamp_ms, uint32_t resetTime_ms)
+void NavEKF2_core::writeExtNavData(const Vector3f &pos, const Quaternion &quat, const float posCovNE[3], float posErrD, float angErr, uint32_t timeStamp_ms, uint32_t resetTime_ms)
 {
-    // protect against NaN
-    if (pos.is_nan() || isnan(posCov[0]) || isnan(posCov[3]) || isnan(posCov[5]) || quat.is_nan() || isnan(angErr)) {
+    // protect against NaN and negtive numbers
+    if (pos.is_nan() || quat.is_nan() || !is_positive(posCovNE[0]) || !is_positive(posCovNE[2]) || !is_positive(posErrD) || !is_positive(angErr)) {
         return;
     }
 
@@ -959,7 +959,7 @@ void NavEKF2_core::writeExtNavData(const Vector3f &pos, const Quaternion &quat, 
 
     extNavDataNew.pos = pos.toftype();
     extNavDataNew.quat = quat.toftype();
-    extNavDataNew.posErr = sqrtf((posCov[0]+posCov[3]+posCov[5]) / 3.0f);
+    extNavDataNew.posErr = sqrtf((posCovNE[0]+posCovNE[2]+sq(posErrD)) / 3.0f);
     extNavDataNew.angErr = angErr;
     timeStamp_ms = timeStamp_ms;
     // Correct for the average intersampling delay due to the filter updaterate
