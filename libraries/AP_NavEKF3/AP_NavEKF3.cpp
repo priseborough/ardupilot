9f92e3bbf9fb20badb5f075ff0d86046af8c4e5e
Paul Riseborough <gncsolns@gmail.com>
2026-01-05 19:06:13 +1100

    AP_NavEKF3: Make it clearer that EK3_DV_YAW_x and EK3_DV_PIT_x are offsets

--- libraries/AP_NavEKF3/AP_NavEKF3.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3.cpp
@@ -742,49 +742,49 @@ const AP_Param::GroupInfo NavEKF3::var_info2[] = {
     AP_GROUPINFO("OPTIONS",  11, NavEKF3, _options, 0),
 
     // @Param: DV_YAW_0
-    // @DisplayName: Learned doppler velocity sensor 0 yaw
+    // @DisplayName: Learned doppler velocity sensor 0 yaw offset
     // @User: Advanced
     // @Units: deg
     AP_GROUPINFO("DV_YAW_0", 12, NavEKF3, _dopplerYawOffset[0], 0.0f),
 
     // @Param: DV_YAW_1
-    // @DisplayName: Learned doppler velocity sensor 1 yaw
+    // @DisplayName: Learned doppler velocity sensor 1 yaw offset
     // @User: Advanced
     // @Units: deg
     AP_GROUPINFO("DV_YAW_1", 13, NavEKF3, _dopplerYawOffset[1], 0.0f),
 
     // @Param: DV_YAW_2
-    // @DisplayName: Learned doppler velocity sensor 2 yaw
+    // @DisplayName: Learned doppler velocity sensor 2 yaw offset
     // @User: Advanced
     // @Units: deg
     AP_GROUPINFO("DV_YAW_2", 14, NavEKF3, _dopplerYawOffset[2], 0.0f),
 
     // @Param: DV_YAW_3
-    // @DisplayName: Learned doppler velocity sensor 3 yaw
+    // @DisplayName: Learned doppler velocity sensor 3 yaw offset
     // @User: Advanced
     // @Units: deg
     AP_GROUPINFO("DV_YAW_3", 15, NavEKF3, _dopplerYawOffset[3], 0.0f),
 
     // @Param: DV_PIT_0
-    // @DisplayName: Learned doppler velocity sensor 0 pitch
+    // @DisplayName: Learned doppler velocity sensor 0 pitch offset
     // @User: Advanced
     // @Units: deg
     AP_GROUPINFO("DV_PIT_0", 16, NavEKF3, _dopplerPitchOffset[0], 0.0f),
 
     // @Param: DV_PIT_1
-    // @DisplayName: Learned doppler velocity sensor 1 pitch
+    // @DisplayName: Learned doppler velocity sensor 1 pitch offset
     // @User: Advanced
     // @Units: deg
     AP_GROUPINFO("DV_PIT_1", 17, NavEKF3, _dopplerPitchOffset[1], 0.0f),
 
     // @Param: DV_PIT_2
-    // @DisplayName: Learned doppler velocity sensor 2 pitch
+    // @DisplayName: Learned doppler velocity sensor 2 pitch offset
     // @User: Advanced
     // @Units: deg
     AP_GROUPINFO("DV_PIT_2", 18, NavEKF3, _dopplerPitchOffset[2], 0.0f),
 
     // @Param: DV_PIT_3
-    // @DisplayName: Learned doppler velocity sensor 3 pitch
+    // @DisplayName: Learned doppler velocity sensor 3 pitch offset
     // @User: Advanced
     // @Units: deg
     AP_GROUPINFO("DV_PIT_3", 19, NavEKF3, _dopplerPitchOffset[3], 0.0f),
